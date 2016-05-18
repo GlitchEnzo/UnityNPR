51179c045e50c94b248f55ae1d22ae9c69b8821a
GlitchEnzo <mccarthp@gmail.com>
2016-05-17 17:04:05 -0700

    Added a RenderTextureReader
    This reads the data output to a RenderTexture.
    
    DOES NOT WORK YET!

---
+++ Assets/Scripts/RenderTextureReader.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class RenderTextureReader : MonoBehaviour
+{
+    public KeyCode keyToRead = KeyCode.R;
+
+    private Camera attachedCamera;
+
+    private RenderTexture attachedRenderTexture;
+
+    void Start()
+    {
+        attachedCamera = GetComponent<Camera>();
+        attachedRenderTexture = attachedCamera.targetTexture;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToRead))
+        {
+            if (attachedRenderTexture != null)
+            {
+                //RenderBuffer buffer = attachedRenderTexture.colorBuffer;
+                //buffer.???
+            }
+        }
+    }
+}
