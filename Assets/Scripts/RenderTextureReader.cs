5495860a1f530f4c9f8352ed62486ffda693997b
GlitchEnzo <mccarthp@gmail.com>
2016-05-18 21:57:26 -0700

    Tried using and reading a Float texture
    DOESN"T WORK!
    
    ReadPixels doesn't work for RFloat type or RInt type:
    "Unsupported texture format for ReadPixels - needs to be ARGB32, RGB24,
    RGBAFloat or RGBAHalf"
    
    GetPixels doesn't work either:
    "Unsupported texture format - needs to be ARGB32, RGBA32, BGRA32, RGB24,
    Alpha8, RGBAFloat, RGBAHalf or one of supported compressed formats."

--- Assets/Scripts/RenderTextureReader.cs
+++ Assets/Scripts/RenderTextureReader.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
 public class RenderTextureReader : MonoBehaviour
@@ -10,47 +9,66 @@ public class RenderTextureReader : MonoBehaviour
 
     private RenderTexture attachedRenderTexture;
 
+    private Texture2D storageTexture;
+    private Rect textureRect;
+
     void Start()
     {
         attachedCamera = GetComponent<Camera>();
         attachedRenderTexture = attachedCamera.targetTexture;
 
-        foreach (TextureFormat format in Enum.GetValues(typeof(TextureFormat)))
-        {
-            try
-            {
-                Debug.LogFormat("TextureFormat.{0} supported = {1}", format, SystemInfo.SupportsTextureFormat(format));
-            }
-            catch
-            {
-                Debug.LogErrorFormat("TextureFormat.{0} was invalid!", format);
-            }
-        }
+        //foreach (TextureFormat format in Enum.GetValues(typeof(TextureFormat)))
+        //{
+        //    try
+        //    {
+        //        Debug.LogFormat("TextureFormat.{0} supported = {1}", format, SystemInfo.SupportsTextureFormat(format));
+        //    }
+        //    catch
+        //    {
+        //        Debug.LogErrorFormat("TextureFormat.{0} was invalid!", format);
+        //    }
+        //}
 
-        Texture2D texture = new Texture2D(1024, 768, TextureFormat.RFloat, false);
-        Color[] colorData = new Color[1024 * 768];
-        for (int x = 0; x < 1024; x++)
-        {
-            for (int y = 0; y < 768; y++)
-            {
-                colorData[y * 1024 + x] = new Color(y * 1024 + x, 0, 0, 0);
-            }
-        }
-        texture.SetPixels(colorData);
+        storageTexture = new Texture2D(1024, 768, TextureFormat.RFloat, false);
+        textureRect = new Rect(0, 0, 1024, 768);
 
-        Debug.Log(texture.GetPixel(0, 0).r + " should = " + (0 * 1024 + 0));
-        Debug.Log(texture.GetPixel(10, 10).r + " should = " + (10 * 1024 + 10));
-        Debug.Log(texture.GetPixel(1023, 767).r + " should = " + (767 * 1024 + 1023));
+        //Color[] colorData = new Color[1024 * 768];
+        //for (int x = 0; x < 1024; x++)
+        //{
+        //    for (int y = 0; y < 768; y++)
+        //    {
+        //        colorData[y * 1024 + x] = new Color(y * 1024 + x, 0, 0, 0);
+        //    }
+        //}
+        //storageTexture.SetPixels(colorData);
+
+        //Debug.Log(storageTexture.GetPixel(0, 0).r + " should = " + (0 * 1024 + 0));
+        //Debug.Log(storageTexture.GetPixel(10, 10).r + " should = " + (10 * 1024 + 10));
+        //Debug.Log(storageTexture.GetPixel(1023, 767).r + " should = " + (767 * 1024 + 1023));
     }
 
     void Update()
     {
         if (Input.GetKeyDown(keyToRead))
         {
-            if (attachedRenderTexture != null)
+            if (attachedCamera != null)
             {
-                //RenderBuffer buffer = attachedRenderTexture.colorBuffer;
-                //buffer.???
+                attachedCamera.Render();
+
+                RenderTexture.active = attachedCamera.targetTexture;
+                
+                storageTexture.ReadPixels(textureRect, 0, 0, false);
+
+                RenderTexture.active = null;
+
+                Color[] data = storageTexture.GetPixels();
+                foreach (var piece in data)
+                {
+                    if (piece.r != 0)
+                    {
+                        Debug.LogFormat("Data = {0}", piece.r);
+                    }
+                }
             }
         }
     }
