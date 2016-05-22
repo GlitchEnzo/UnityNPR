7dc1706baaca28d80c5d90eb67a2a9773a5f2b03
GlitchEnzo <mccarthp@gmail.com>
2016-05-21 17:09:17 -0700

    Added projection space rectangle creator
    Frustum culling cannot be disabled in Unity, so the object must still be
    placed in the camera's view frustum in order to render.

---
+++ Assets/Scripts/ProjectionSpaceRect.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectionSpaceRect : MonoBehaviour
+{
+    public Mesh rectangle;
+    public Material material;
+    
+    void Start()
+    {
+        // 2 --- 3
+        // |     |
+        // |     |
+        // 0-----1
+        
+        Vector3[] vertexBuffer = new Vector3[] { new Vector3(-0.5f, -0.5f, 0),
+                                                 new Vector3( 0.5f, -0.5f, 0),
+                                                 new Vector3(-0.5f,  0.5f, 0),
+                                                 new Vector3( 0.5f,  0.5f, 0) };
+
+        Color32[] colorBuffer = new Color32[] { new Color32(255,   0,   0, 255),
+                                                new Color32(  0, 255,   0, 255),
+                                                new Color32(  0,   0, 255, 255),
+                                                new Color32(255, 255, 255, 255) };
+
+        Vector2[] uvBuffer = new Vector2[] { new Vector2(0, 0),
+                                             new Vector2(1, 0),
+                                             new Vector2(0, 1),
+                                             new Vector2(1, 1) };
+
+        int[] indexBuffer = new int[] { 1, 0, 2, 2, 3, 1};
+
+        rectangle = new Mesh();
+        rectangle.vertices = vertexBuffer;
+        rectangle.colors32 = colorBuffer;
+        rectangle.uv = uvBuffer;
+        rectangle.triangles = indexBuffer;
+
+        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+        meshFilter.sharedMesh = rectangle;
+
+        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        meshRenderer.sharedMaterial = material;
+    }
+}
