8f738e907b930219835527d360e412a2166ad463
GlitchEnzo <mccarthp@gmail.com>
2016-05-30 21:10:45 -0700

    Changed the color encoding to not use alpha
    This makes the colors easier for debugging.

--- Assets/Scripts/ColorConversion.cs
+++ Assets/Scripts/ColorConversion.cs
@@ -4,26 +4,32 @@ public static class ColorConversion
 {
     public static int Color32ToInt(Color32 color)
     {
-        if (color.r > 127)
+        if (color.a != 0xFF)
         {
-            Debug.LogWarning("Red channel cannot be larger than 127 (7-bits) for positive integers.  8th bit is the sign bit.");
+            Debug.LogWarning("Alpha channel is expected to always be 256");
         }
 
-        int value = color.a;
-        value += color.b << 8;
-        value += color.g << 16;
-        value += color.r << 24;
+        int value = color.b;
+        value += color.g << 8;
+        value += color.r << 16;
+        //value += color.a << 24;
 
         return value;
     }
 
     public static Color32 IntToColor32(int value)
     {
-        // [red] [green] [blue] [alpha]
-        byte r = (byte)((value >> 24) & 0xFF);
-        byte g = (byte)((value >> 16) & 0xFF);
-        byte b = (byte)((value >> 8) & 0xFF);
-        byte a = (byte)((value) & 0xFF);
+        if (value > System.Math.Pow(2, 24))
+        {
+            Debug.LogWarning("Maximum value allowed is 2^24 = 16,777,216.  This is because the alpha channel is forced to 256");
+        }
+
+        // [alpha] [red] [green] [blue] 
+        //byte a = (byte)((value >> 24) & 0xFF);
+        byte a = 0xFF;
+        byte r = (byte)((value >> 16) & 0xFF);
+        byte g = (byte)((value >> 8) & 0xFF);
+        byte b = (byte)((value) & 0xFF);
 
         return new Color32(r, g, b, a);
     }
